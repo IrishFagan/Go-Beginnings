e56aa0e2ca4b5fb167738e0b2dce7f4a8f42edf2
Irish Fagan <IrishFagan@live.com>
2019-10-21 18:28:10 -0700

    Added a few lines to print some stuff!

--- spaceadventure.go
+++ spaceadventure.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Works!")
+	fmt.Println("Welcome to our quaint solar system!")
+	fmt.Println("There used to be 9 planets but now there are only 8...")
+	fmt.Println("...")
+	fmt.Println("..anyways, what can I call you?\n")
 
-	fmt.Println("this works too!")
+
 }
