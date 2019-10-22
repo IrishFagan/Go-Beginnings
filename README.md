c43252372d76b149424637a460f257552596eecd
Irish Fagan <IrishFagan@live.com>
2019-10-21 17:57:17 -0700

    Initial commit!

---
+++ README.md
@@ -0,0 +1,3 @@
+##Go-Beginnings!
+
+This is my first attempt at learning the Go programming language!
---
+++ spaceadventure.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Works!")
+
+	fmt.Println("this works too!")
+}
