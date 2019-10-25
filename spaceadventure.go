b0181411ab309a5fe2fe50eeea05a9ce93dd421b
Irish Fagan <IrishFagan@live.com>
2019-10-25 13:46:49 -0700

    Reworked structure of program. Still have much that could be reformatted!

--- spaceadventure.go
+++ spaceadventure.go
@@ -9,84 +9,76 @@ import (
 	"os"
 )
 
-//JSON Structs
-type Planets struct {
-	Planets []Planet `json: "planets"`
-}
-
-
 type Planet struct {
 	Name string `json: "name"`
 	Description string `json: "description"`
 }
 
-//Only function(I realize how ugly this looks but I felt too deep to reformat into indivindual functions. Will go back and fix
-//so that it's not as ugly)
+type Planets struct {
+	Planets []Planet `json: "planets"`
+}
+
 func main() {
+	conversation()
+}
+
+func conversation() {	
 	file, _ := ioutil.ReadFile("planetarySystem.json")
-	data := Planets{}
+	p_data := Planets{}
+	_ = json.Unmarshal([]byte(file), &p_data)
 
-	_ = json.Unmarshal([]byte(file), &data)
-
-	//Ask for name and if user would like a random planet
+	var nameInput string
 	fmt.Println("Welcome to our quaint solar system!")
 	fmt.Println("There used to be 9 planets but now there are only 8...")
 	fmt.Println("...")
 	fmt.Println("..anyways, what would you prefer I call you?\n")
 	fmt.Println("ENTER NAME PLS: ")
-	var nameInput string
 	fmt.Scanln(&nameInput)
 	fmt.Println("\nHmm It's a pleasure to meet you, " + nameInput)
 	fmt.Println("My name is Gort, and I'm here to show you around our solar system!")
+
+	handleInput(p_data)
+}
+
+func handleInput(p_data Planets) {
 	var randPlanet string
 	fmt.Println("Would you like to hear about a random planet?")
 	fmt.Println("Y or N")
 	fmt.Scanln(&randPlanet)
 
-	//take input Y or N
-	for {
-		if randPlanet == "Y" {
-			rand.Seed(time.Now().UnixNano())
-			var randInt = rand.Intn(len(data.Planets))
-			fmt.Println(randInt)
-			fmt.Println("Name: " + data.Planets[randInt].Name)
-			fmt.Println("Desc: " + data.Planets[randInt].Description)
-			os.Exit(3)
-		} else if randPlanet == "N" {
-			break
-		} else if randPlanet != "Y" {
-			fmt.Println("I didn't catch that.")
-			fmt.Println("Would you like to hear about a random planet?")
-			fmt.Scanln(&randPlanet)
-		} else if randPlanet != "N" {
-			fmt.Println("I didn't catch that.")
-			fmt.Println("Would you like to hear about a random planet?")
-			fmt.Scanln(&randPlanet)
-		}
+	
+	if randPlanet == "Y" {
+		rand.Seed(time.Now().UnixNano())
+		var randInt = rand.Intn(len(p_data.Planets))
+		fmt.Println("Name: " + p_data.Planets[randInt].Name)
+		fmt.Println("Desc: " + p_data.Planets[randInt].Description)
+		os.Exit(3)
+	} else if randPlanet == "N" {
+		specificPlanet(p_data)
+	} else if randPlanet != "Y" {
+		handleInput(p_data)
+	} else if randPlanet != "N" {
+		handleInput(p_data)
 	}
+}
 
-	//handle input N
-	if randPlanet == "N" {
-		fmt.Println("Alright, then what planet would you like to hear about?")
-	}
+func specificPlanet(p_data Planets) {
 	var planetInput string
 	var planetFound bool
+	
+	fmt.Println("Alright, then what planet would you like to hear about?")
 	fmt.Scanln(&planetInput)
 
-	for i := 0; i < len(data.Planets); i++ {
-		if planetInput == data.Planets[i].Name {
+	for i := 0; i < len(p_data.Planets); i++ {
+		if planetInput == p_data.Planets[i].Name {
 			fmt.Println(planetInput)
-			fmt.Println(data.Planets[i].Description)
+			fmt.Println(p_data.Planets[i].Description)
 			planetFound = true
 		}
 	}
 
 	if planetFound != true {
 		fmt.Print("Sorry, I don't know that planet!")
+		specificPlanet(p_data)
 	}
-
-	//program doesn't ask again if input doesn't match acutal planet. Need to reformat when I actually
-	//reformat into indivindual functions since that will make the implementation a lot less messy
-
-	fmt.Print("\n")
 }
